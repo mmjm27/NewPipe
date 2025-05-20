b76f7ebd596a762b7897c077ba17fef7c3e62153
mmjm27 <max.matero@hotmail.com>
2025-05-20 21:10:42 +0300

    Forgot git add -A

--- app/src/main/java/org/schabi/newpipe/ktx/SharedPreferences.kt
+++
@@ -1,7 +0,0 @@
-package org.schabi.newpipe.ktx
-
-import android.content.SharedPreferences
-
-fun SharedPreferences.getStringSafe(key: String, defValue: String): String {
-    return getString(key, null) ?: defValue
-}
